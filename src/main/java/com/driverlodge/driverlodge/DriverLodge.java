e8fbe9b2a19f22890c56625b7938692fe28bf74e
MaxmilianEl <maximilian.elbers@hs-osnabrueck.de>
2022-06-01 08:53:47 +0200

    Icon Added

--- src/main/java/com/driverlodge/driverlodge/DriverLodge.java
+++ src/main/java/com/driverlodge/driverlodge/DriverLodge.java
@@ -3,6 +3,7 @@ package com.driverlodge.driverlodge;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,8 +12,12 @@ public class DriverLodge extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(DriverLodge.class.getResource("startUpView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+        Image image = new Image(getClass().getResource("images/LodgeImage.png").toExternalForm());
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
         stage.setTitle("DriverLodge");
+        stage.setResizable(false);
+        stage.getIcons().add(image);
         stage.setScene(scene);
         stage.show();
 
